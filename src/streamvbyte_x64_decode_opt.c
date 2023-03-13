ab813ea05fa8fb2e0efa6f8a9cf1c9b3e933bd4f
Anson Thai <at9562@gadi-login-04.gadi.nci.org.au>
2023-03-13 22:16:08 +1100

    restructure, working

--- src/streamvbyte_x64_decode_opt.c
+++ src/streamvbyte_x64_decode_opt.c
@@ -1,29 +1,35 @@
 #include "streamvbyte_isadetection.h"
 #ifdef STREAMVBYTE_X64
+
+
+
 STREAMVBYTE_TARGET_SSE41
-static inline __m128i _decode_sse41_opt(uint32_t key,
-                                  const uint8_t *__restrict__ *dataPtrPtr) {
+static inline void _write_sse41_opt(uint32_t *out, __m128i Vec) {
+  _mm_storeu_si128((__m128i *)out, Vec);
+}
+STREAMVBYTE_UNTARGET_REGION
+
+
+STREAMVBYTE_TARGET_SSE41
+static inline void _decode_sse41_opt(uint8_t **key,
+                                  const uint8_t *__restrict__ *dataPtrPtr, uint32_t **outPtrPtr) {
   uint8_t len;
   __m128i Data = _mm_loadu_si128((__m128i *)*dataPtrPtr);
-  uint8_t *pshuf = (uint8_t *) &shuffleTable[key];
+  uint8_t *pshuf = (uint8_t *) &shuffleTable[**key];
   __m128i Shuf = *(__m128i *)pshuf;
 #ifdef AVOIDLENGTHLOOKUP
   // this avoids the dependency on lengthTable,
   // see https://github.com/lemire/streamvbyte/issues/12
   len = pshuf[12 + (key >> 6)] + 1;
 #else
-  len = lengthTable[key];
+  len = lengthTable[**key];
 #endif
   Data = _mm_shuffle_epi8(Data, Shuf);
   *dataPtrPtr += len;
-  return Data;
-}
-STREAMVBYTE_UNTARGET_REGION
 
-
-STREAMVBYTE_TARGET_SSE41
-static inline void _write_sse41_opt(uint32_t *out, __m128i Vec) {
-  _mm_storeu_si128((__m128i *)out, Vec);
+  _write_sse41_opt(*outPtrPtr, Data);
+  *outPtrPtr += 4;
+  *key += 1;
 }
 STREAMVBYTE_UNTARGET_REGION
 
@@ -37,15 +43,11 @@ const uint8_t *svb_decode_sse41_simple_opt(uint32_t *out,
 
   uint64_t keybytes = count / 4; // number of key bytes
 
-  const uint8_t *keyPtr8 = (const uint8_t *)keyPtr;
+  uint8_t *keyPtr8 = (uint8_t *)keyPtr;
 
-  __m128i Data;
   if (keybytes >= 1) {
     for (; keybytes > 0; keybytes--) {
-      Data = _decode_sse41_opt(*keyPtr8, &dataPtr);
-      _write_sse41_opt(out, Data);
-      keyPtr8++;
-      out += 4;
+      _decode_sse41_opt(&keyPtr8, &dataPtr, &out);
     }
   }
   return dataPtr;
